f5396262b24935d3bb1ea603480bd24789f58363
Arielzamler <ariel.zamlerr@gmail.com>
2025-04-13 16:46:00 +0300

    Final correct CORS setup

--- api/generate-song.js
+++ api/generate-song.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     // Proper CORS setup
-    res.setHeader('Access-Control-Allow-Origin', 'https://vkgte4-6f.myshopify.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
